29a961d0b563877f4eb210790df05281f222b2f6
Aneesh Sharma <Aneesh@gmail.com>
2019-01-19 19:15:46 +0530

    add async chunks for components

--- src/App.js
+++ src/App.js
@@ -1,31 +1,48 @@
 import React, { Component } from 'react';
+import asyncComponent from './hoc/asyncComponent/asyncComponent.js'
 import Layout from './components/Layout/Layout';
-import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
-import Checkout from './containers/checkout/checkout.js';
 import {Route,Switch,withRouter,Redirect} from 'react-router-dom';
-import Orders from './containers/Orders/Order.js';
-import Auth from './containers/Auth/Auth.js';
-import LogOut from './containers/Auth/LogOut/LogOut.js';
 import * as actions from './store/actions/index.js';
 import {connect} from 'react-redux';
+
+const asyncCheckout=asyncComponent(()=>{
+  return import('./containers/checkout/checkout.js');
+});
+
+const asyncBurgerBuilder=asyncComponent(()=>{
+  return import('./containers/BurgerBuilder/BurgerBuilder');
+});
+
+const asyncOrders=asyncComponent(()=>{
+  return import('./containers/Orders/Order.js');
+});
+
+const asyncAuth=asyncComponent(()=>{
+  return import('./containers/Auth/Auth.js');
+});
+
+const asyncLogOut=asyncComponent(()=>{
+  return import('./containers/Auth/LogOut/LogOut.js');
+});
+
 class App extends Component {
 	componentDidMount(){
    this.props.onauthCheckState();
  }
  render() {
- let  routes=( <Switch>
-    <Route path="/" exact component={BurgerBuilder}/>
-    <Route path="/auth" component={Auth}/>
+   let  routes=( <Switch>
+    <Route path="/" exact component={asyncBurgerBuilder}/>
+    <Route path="/auth" component={asyncAuth}/>
     <Redirect to="/"/>
     </Switch>
     );
-  if(this.props.isauth){
+   if(this.props.isauth){
     routes=( <Switch>
-      <Route path="/orders" component={Orders}/>
-      <Route path="/checkout" component={Checkout}/>
-       <Route path="/auth" component={Auth}/>
-      <Route path="/logOut" exact component={LogOut}/>
-      <Route path="/" exact component={BurgerBuilder}/>
+      <Route path="/orders" component={asyncOrders}/>
+      <Route path="/checkout" component={asyncCheckout}/>
+      <Route path="/auth" component={asyncAuth}/>
+      <Route path="/logOut" exact component={asyncLogOut}/>
+      <Route path="/" exact component={asyncBurgerBuilder}/>
       <Redirect to="/"/>
       </Switch>);
   }
